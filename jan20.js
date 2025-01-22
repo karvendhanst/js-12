513ea849d0505e47b90df9f115fd48c9f9510349
KARVENDHAN <imkarvendhan@gmail.com>
2025-01-22 13:27:13 +0530

    Create jan20.js

---
+++ jan20.js
@@ -0,0 +1,36 @@
+// let firstName = "naveen" ,lastName = "kumar";
+
+// console.log("Firstname: " + firstName ,"Secondname: " + lastName);
+
+// console.log("Hi i'm Gokul");
+
+// // Template Literals
+
+// let uname = "GokulNath";
+
+// console.log(`Hi i'm ${uname} ${1+2}`);
+
+// {
+//     const a =10;
+//     a=20;
+//     console.log(a);
+    
+// }
+
+// {
+//     let a = 10;
+//     console.log(a);
+// }
+
+// { 
+//     console.log(a);
+// }
+
+let a = 10;
+let b = a;
+
+console.log(b);
+
+
+
+
