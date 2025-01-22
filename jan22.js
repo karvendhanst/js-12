7e47836436958e58fe59e650d4d7d8efbe5b1f49
KARVENDHAN <imkarvendhan@gmail.com>
2025-01-22 13:27:51 +0530

    Create jan22.js

---
+++ jan22.js
@@ -0,0 +1,59 @@
+// Datatypes
+
+// Number
+let a = 5;
+
+console.log(typeof a);
+
+// String
+
+let user = 'Vinoth 1+1'
+
+console.log(typeof user);
+
+let userName = "Naveen"
+
+console.log("Hello" + " " + userName);
+console.log(`Hello ${userName} kumar`);
+
+// Boolean
+
+let isMale = false;
+console.log(typeof isMale);
+
+// Null
+
+let data = null;
+
+console.log( data);
+
+// Undefined
+
+let data1;
+
+console.log(data1);
+
+// alert("Hi im alert")
+
+// let userData = prompt("Enter user name : ")
+// console.log(userData);
+
+// let status = confirm("Are you single ? ")
+// console.log(status);
+
+let name = prompt("Enter Your Name : ")
+
+let result = confirm(`Are you sure your name is ${name}`)
+
+console.log(`${name} is ${result}`);
+
+
+
+
+
+
+
+
+
+
+
