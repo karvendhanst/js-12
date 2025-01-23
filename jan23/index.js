6e27a12d7f63cceefe2b82979cbe483113890e00
KARVENDHAN <imkarvendhan@gmail.com>
2025-01-23 11:25:29 +0300

    Create index.js

---
+++ jan23/index.js
@@ -0,0 +1,57 @@
+// let a = 5;
+
+// Pre increment
+// console.log(--a); // 4
+// Post increment
+// console.log(a--); // 5
+
+// console.log(a**2); // 25
+
+
+// let a = 5;
+
+// let b = a + 5;
+
+// console.log(b);
+
+// let x = 10;
+
+// x = x+5;
+// x+=5;
+
+// console.log(x);
+
+// let a = "2";
+// let b = 2;
+
+// console.log(a == b);
+
+// it checks only value
+
+// let x = "10";
+// let y = 10;
+
+// console.log(x === y);
+// it checks a value & datatype
+
+// let s = 20;
+// let t = 20;
+
+// console.log(s != t);
+
+
+// console.log(10>=10);
+
+
+// let a =10;
+// let b = 20;
+
+// console.log(a == b || a != b);
+
+let x = 10;
+let y = 12;
+
+// console.log(x & y);
+
+console.log(x | y);
+
